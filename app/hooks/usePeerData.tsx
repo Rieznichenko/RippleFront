fa41f4d3d9ed16a7d2cca0b1f54b9c319059d4ac
tariqMah <t.ali1993@hotmail.com>
2024-02-01 12:38:05 +0400

    add country flag

--- app/hooks/usePeerData.tsx
+++ app/hooks/usePeerData.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useMemo, useState } from "react";
 import Lottie from "lottie-react";
 
 import offlineAnimationData from "@/components/lottie/wired-flat-64-wifi-offline.json";
+import ReactCountryFlag from "react-country-flag";
+
+import inboundState from "@/components/lottie/wired-outline-33-arrow-down.json";
+import outboundState from "@/components/lottie/wired-outline-34-arrow-up.json";
 
 const usePeerData = () => {
   const column = useMemo(
@@ -45,9 +49,42 @@ const usePeerData = () => {
     fetch("https://xrpkuwait.com/peerinfo")
       .then((response) => response.json())
       .then((data: any) => {
-        console.log("SERVER", data);
-        let modifyData: any = [];
-        setPeers(data);
+        let modifyData = data?.map((item: any, index: any) => {
+          return {
+            pubkey: item?.pubkey,
+            country: (
+              <ReactCountryFlag
+                key={item.pubkey}
+                countryCode={item.country}
+                svg
+                title={item.country}
+                style={{ width: 30, height: 30 }}
+              />
+            ),
+            version: item?.version,
+            serverState: item?.serverState,
+            direction:
+              item.direction == "inbound" ? (
+                <Lottie
+                  animationData={inboundState}
+                  loop={true}
+                  autoplay={true}
+                  style={{ width: 30, height: 30, margin: "0 auto" }}
+                />
+              ) : (
+                <Lottie
+                  animationData={outboundState}
+                  loop={true}
+                  autoplay={true}
+                  style={{ width: 30, height: 30, margin: "0 auto" }}
+                />
+              ),
+            latency: item?.latency,
+            ledgers: item?.ledgers || "N/A",
+          };
+        });
+        console.log("🚀 ~ .then ~ modifyData:", modifyData);
+        setPeers(modifyData);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
